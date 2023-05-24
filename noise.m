e8c5b0fa205e371307775b215305ca831bb6d374
AtharvTambade <115065219+AtharvTambade@users.noreply.github.com>
2023-05-24 23:24:12 +0530

    Add files via upload

--- noise.m
+++ noise.m
@@ -1,6 +1,6 @@
-function quaternion_noise = noise(quaternion,sigma_theta,sigma_vector)
+function quaternion_noise_norm = noise(quaternion,sigma_theta,sigma_vector)
 if quaternion(1)==1
-    quaternion_noise=[1,0,0,0];
+    quaternion_noise_norm=[1,0,0,0];
 else
     theta=2*acosd(quaternion(1));
     theta_radian=theta*pi/180;
@@ -17,6 +17,7 @@ else
     noise_beta_z_radian=noise_beta_z*pi/180;
     noise_theta_radian=noise_theta*pi/180;
     quaternion_noise=[cos(noise_theta_radian/2),sin(noise_theta_radian/2)*cos(noise_beta_x_radian),sin(noise_theta_radian/2)*cos(noise_beta_y_radian),sin(noise_theta_radian/2)*cos(noise_beta_z_radian)];
+    quaternion_noise_norm = quaternion_noise/norm(quaternion_noise);
 end
 end
     
